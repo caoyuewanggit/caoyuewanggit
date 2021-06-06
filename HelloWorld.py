53f605f93ec13f281835f50bc960167e19ae124f
caoyuewang <caoyuewang@foxmail.com>
2021-06-06 20:13:35 +0800

    Default Changelist

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,9 @@
+import time
+from selenium import webdriver
+browser=webdriver.Chrome()
+browser.get("http://www.baidu.com")
+time.sleep(5)
+browser.quit()
 print("hello worl1d")
 print("哈哈")
-print("你好不好")
+print("你好不好")                        
